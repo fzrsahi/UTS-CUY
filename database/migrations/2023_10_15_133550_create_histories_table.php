92676189bfa8a10f1e4358226b24c8f689c4e73f
fzrsahi <fazrul.anugrah17@gmail.com>
2023-10-15 21:42:47 +0700

    fix : typo in model and migrations

--- database/migrations/2023_10_15_133550_create_histories_table.php
+++ database/migrations/2023_10_15_133550_create_histories_table.php
@@ -22,7 +22,6 @@ return new class extends Migration
             $table->integer("qty");
             $table->foreignIdFor(user::class)->constrained();
             $table->foreignIdFor(supplier::class)->constrained();
-            $table->timestamp('added_on')->nullable()->default(time());
             $table->timestamps();
         });
     }
